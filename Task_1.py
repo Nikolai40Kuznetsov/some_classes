d31112ca39a63389cfc819924696eb1f742d5b1d
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-03-11 15:11:28 +0300

    Update Task_1.py
    база уравнения, условия графиков

--- Task_1.py
+++ Task_1.py
@@ -7,5 +7,13 @@ class Square_equation:
         
     def draw(self):
         pass
-        
+
+class Derivative:
+    
+    #2ax + b
+    
+class Integrale:
+    
+    #a(x^3)/3 + b(x^2)/2 + cx + d
+       
 Equation = Square_equation(1, 2, 1)
