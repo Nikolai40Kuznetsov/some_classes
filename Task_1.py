0ed363ff830b0241ef48e439135c99a9ad6f6998
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-03-11 14:59:36 +0300

    Create Task_1.py
    Квадратное уравнение (база)

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+class Square_equation:
+    
+    def __init__(self, par_a, par_b, par_c):
+        self.par_a = par_a
+        self.par_b = par_b
+        self.par_c = par_c
+        
+    def draw(self):
+        pass
+        
+Equation = Square_equation(1, 2, 1)
